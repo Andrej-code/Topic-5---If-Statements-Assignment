b2b9c3f6e63e4dc6cbf3cae575c496b63869cdfe
Andrej Trajkovic <700007609@GoTVDSB.ca>
2025-09-19 09:24:07 -0400

    kind of getting done

--- Topic 5 - If Statements Assignment/Program.cs
+++ Topic 5 - If Statements Assignment/Program.cs
@@ -34,52 +34,66 @@
 
             // #2 - Parking Garage Cost
 
-
             int numOfMinutes;
-            double parkingCost;
-
-            parkingCost = 2;
-            
             Console.WriteLine("How long did you leave your car for at the parking garage?");
             int.TryParse(Console.ReadLine(), out numOfMinutes);
 
-            if (numOfMinutes < 60)
+            //    // 1 hour
+            if (numOfMinutes <= 60)
             {
                 Console.WriteLine("You are only charged for $4.00");
             }
-            else if (numOfMinutes > 60 && numOfMinutes < 120)
+            //    // 2 hours
+            else if (numOfMinutes > 60 && numOfMinutes <= 120)
             {
                 Console.WriteLine("You are charged for $6.00");
             }
-            else if (numOfMinutes > 120 && numOfMinutes < 180)
+            //    // 3 hours
+            else if (numOfMinutes > 120 && numOfMinutes <= 180)
             {
                 Console.WriteLine("You are charged for $8.00");
             }
-            else if (numOfMinutes > 180 && numOfMinutes < 240)
+            //    // 4 hours
+            else if (numOfMinutes > 180 && numOfMinutes <= 240)
             {
                 Console.WriteLine("You are charged for $10.00");
             }
-            else if (numOfMinutes > 240 && numOfMinutes < 300)
+            //    // 5 hours
+            else if (numOfMinutes > 240 && numOfMinutes <= 300)
             {
                 Console.WriteLine("You are charged for $12.00");
             }
-            else if (numOfMinutes > 300 && numOfMinutes < 360)
+            //    // 6 hours
+            else if (numOfMinutes > 300 && numOfMinutes <= 360)
             {
                 Console.WriteLine("You are charged for $14.00");
             }
-            else if (numOfMinutes > 360 && numOfMinutes < 420)
+            //    // 7 hours
+            else if (numOfMinutes > 360 && numOfMinutes <= 420)
             {
                 Console.WriteLine("You are charged for $16.00");
             }
-            else if (numOfMinutes > 420 && numOfMinutes < 480)
+            //    // 8 hours
+            else if (numOfMinutes > 420 && numOfMinutes <= 480)
             {
                 Console.WriteLine("You are charged for $18.00");
             }
-            else if (numOfMinutes > 480 && numOfMinutes <  540)
+            //    // 9 hours
+            else if (numOfMinutes > 480 && numOfMinutes <= 540)
             {
                 Console.WriteLine("You are charged for $20.00");
             }
-           
+
+            // #3 - Huricane
+
+            // Catagory 1: 74-95 mph, 64-82 kt, 110-153 km/hr
+            // Catagory 2: 96-110 mph, 83-95 kt, 154-177 km/hr
+            // Catagory 3: 111-130 mph, 96-113 kt, 178-209 km/hr
+            // Catagory 4: 131-155 mph, 114-135 kt, 210-249 km/hr
+            // Catagory 5: > 155 mph, 135 kt, 249 km/hr
+
+
+
         }
     }
 }
